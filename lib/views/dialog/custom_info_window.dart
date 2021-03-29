21fbe2397c27cdd0f0a456d87eb1e6d4d5177ac1
Grigory Kiladze <ryhor.kiladze@quandoo.com>
2021-03-29 17:39:22 +0200

    animation basis

--- lib/views/dialog/custom_info_window.dart
+++ lib/views/dialog/custom_info_window.dart
@@ -12,7 +12,24 @@ class CustomInfoWindow extends StatefulWidget {
   State<StatefulWidget> createState() => _CustomInfoWindowState();
 }
 
-class _CustomInfoWindowState extends State<CustomInfoWindow> {
+class _CustomInfoWindowState extends State<CustomInfoWindow> with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation<double> scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    // TODO improve animation
+    controller = AnimationController(vsync: this, duration: Duration(milliseconds: 950));
+    scaleAnimation = CurvedAnimation(parent: controller, curve: Curves.elasticInOut);
+
+    controller.addListener(() {
+      setState(() {});
+    });
+
+    controller.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -20,75 +37,82 @@ class _CustomInfoWindowState extends State<CustomInfoWindow> {
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(Constants.padding),
         ),
-        child: Stack(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.only(
-                  left: Constants.padding,
-                  top: Constants.avatarRadius,
-                  right: Constants.padding,
-                  bottom: Constants.padding),
-              margin: EdgeInsets.only(
-                  left: Constants.padding,
-                  top: Constants.avatarRadius,
-                  right: Constants.padding,
-                  bottom: Constants.padding),
-              decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(Constants.padding),
-                  boxShadow: [
-                    BoxShadow(color: Colors.black, offset: Offset(0, 10), blurRadius: 10),
-                  ]),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Text(
-                    widget.chatUser.name,
-                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Text(
-                    "I'd like to chat",
-                    style: TextStyle(fontSize: 14),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Align(
-                    alignment: Alignment.bottomRight,
-                    child: FlatButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/chat', arguments: widget.chatUser);
-                        },
-                        child: Text(
-                          "Chat",
-                          style: TextStyle(fontSize: 18),
-                        )),
-                  ),
-                ],
-              ),
-            ),
-            Positioned(
+        child: ScaleTransition(
+          scale: scaleAnimation,
+          child: _getInfoWindow(),
+        ));
+  }
+
+  _getInfoWindow() {
+    return Stack(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.only(
               left: Constants.padding,
+              top: Constants.avatarRadius,
               right: Constants.padding,
-              child: CircleAvatar(
-                backgroundColor: Colors.transparent,
-                radius: Constants.avatarRadius,
-                child: ClipRRect(
-                    borderRadius: BorderRadius.all(Radius.circular(Constants.avatarRadius)),
-                    child: Icon(
-                      Icons.emoji_emotions,
-                      size: 64.0,
-                      // color: Colors.orange,
+              bottom: Constants.padding),
+          margin: EdgeInsets.only(
+              left: Constants.padding,
+              top: Constants.avatarRadius,
+              right: Constants.padding,
+              bottom: Constants.padding),
+          decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(Constants.padding),
+              boxShadow: [
+                BoxShadow(color: Colors.black, offset: Offset(0, 10), blurRadius: 10),
+              ]),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(
+                widget.chatUser.name,
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                "I'd like to chat",
+                style: TextStyle(fontSize: 14),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: FlatButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/chat', arguments: widget.chatUser);
+                    },
+                    child: Text(
+                      "Chat",
+                      style: TextStyle(fontSize: 18),
                     )),
               ),
-            ),
-          ],
-        ));
+            ],
+          ),
+        ),
+        Positioned(
+          left: Constants.padding,
+          right: Constants.padding,
+          child: CircleAvatar(
+            backgroundColor: Colors.transparent,
+            radius: Constants.avatarRadius,
+            child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(Constants.avatarRadius)),
+                child: Icon(
+                  Icons.emoji_emotions,
+                  size: 64.0,
+                  // color: Colors.orange,
+                )),
+          ),
+        ),
+      ],
+    );
   }
 }
 
